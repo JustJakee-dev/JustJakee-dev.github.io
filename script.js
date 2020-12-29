019a59e67c2ec1ed075e8d5726b837bce64d915e
JustJakee-dev <76749156+JustJakee-dev@users.noreply.github.com>
2020-12-29 16:49:18 -0500

    More rows

---
+++ script.js
@@ -0,0 +1,16 @@
+let rowImg;
+
+function XX() {
+	var numberOfRows = document.getElementById(Rows).value;
+}
+
+function numberOfRows(Rows) {
+	img = loadImage('Images/Rows.jpg')
+	if (true) {numberOfRows = 1}
+		image(img, 0, 0)
+	if (true) {numberOfRows = 2}
+		image(img, 0, 0)
+		image(img. 55, 0)
+
+
+}
